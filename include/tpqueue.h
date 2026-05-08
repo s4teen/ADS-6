701b9d9ab53d6f8be9481e334d9dea3ae44248f9
s4teen <112770218+s4teen@users.noreply.github.com>
2026-05-08 17:23:26 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,49 +4,61 @@
 
 template<typename T>
 class TPQueue {
-  private:
-    struct Node {
-        T data;
-        Node* next;
-    };
-    Node* head;
-    Node* create(const T& data) {
-        return new Node{data, nullptr};
+ private:
+  struct Node {
+    T data;
+    Node* next;
+  };
+
+  Node* head;
+
+  Node* create(const T& data) {
+    return new Node{data, nullptr};
+  }
+
+ public:
+  TPQueue() : head(nullptr) {}
+
+  ~TPQueue() {
+    while (head) {
+      Node* temp = head;
+      head = head->next;
+      delete temp;
+    }
+  }
+
+  void push(const T& data) {
+    Node* item = create(data);
+
+    if (!head || data.prior > head->data.prior) {
+      item->next = head;
+      head = item;
+      return;
     }
 
-public:
-    TPQueue() : head(nullptr) {}
-    ~TPQueue() {
-        while (head) {
-            Node* temp = head;
-            head = head->next;
-            delete temp;
-        }
+    Node* cur = head;
+
+    while (cur->next && cur->next->data.prior >= data.prior) {
+      cur = cur->next;
     }
-    void push(const T& data) {
-        Node* item = create(data);
-        if (!head || data.prior > head->data.prior) {
-            item->next = head;
-            head = item;
-            return;
-        }
-        Node* cur = head;
-        while (cur->next && cur->next->data.prior >= data.prior) {
-            cur = cur->next;
-        }
-        item->next = cur->next;
-        cur->next = item;
+
+    item->next = cur->next;
+    cur->next = item;
+  }
+
+  T pop() {
+    if (!head) {
+      throw "PQueue empty!";
     }
-    T pop() {
-        if (!head) {
-            throw "PQueue empty!";
-        }
-        Node* temp = head;
-        T data = head->data;
-        head = head->next;
-        delete temp;
-        return data;
-    }
+
+    Node* temp = head;
+    T data = head->data;
+
+    head = head->next;
+    delete temp;
+
+    return data;
+  }
 };
 
 struct SYM {
